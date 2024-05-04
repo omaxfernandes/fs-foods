3f1c967fbf5e42930d5f21f5439fc7c720a0fb6a
omaxfernandes <max.sfernandes@outlook.com>
2024-05-04 03:24:56 -0300

    feat(header): add initial hearder component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image'
+import Header from './_components/ui/header'
 
 export default function Home() {
-  return <h1>fs delivery foods</h1>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
