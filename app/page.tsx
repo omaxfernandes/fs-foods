57ab0d03eccfb4a7eb068749b5fcc7d1ea46403c
omaxfernandes <max.sfernandes@outlook.com>
2024-05-04 04:34:45 -0300

    feat(categories): adds category item and category list

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Header from './_components/header'
 import Search from './_components/search'
+import CategoryList from './_components/category-list'
 
 export default function Home() {
   return (
@@ -8,6 +9,9 @@ export default function Home() {
       <Header />
       <div className="px5 pt-6">
         <Search />
+      </div>
+      <div className="px-5 pt-6">
+        <CategoryList />
       </div>
     </>
   )
