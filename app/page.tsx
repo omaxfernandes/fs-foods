05ff55c21eec18c8a055cdd322df6d09f6a95312
omaxfernandes <max.sfernandes@outlook.com>
2024-05-04 03:48:06 -0300

    feat(search): add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
-import Header from './_components/ui/header'
+import Header from './_components/header'
+import Search from './_components/search'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div className="px5 pt-6">
+        <Search />
+      </div>
     </>
   )
 }
